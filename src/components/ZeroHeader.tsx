c4113d49a14d060b9b8613153aeaa824da6942b1
ryo yoneyama <uryonet@gmail.com>
2020-08-04 22:19:53 +0900

    ヘッダーのコンテンツを整理

--- src/components/ZeroHeader.tsx
+++ src/components/ZeroHeader.tsx
@@ -1,22 +1,29 @@
 import React from 'react'
-import { Layout, Select, Avatar } from 'antd'
+import { Layout, Select, Avatar, Dropdown, Menu } from 'antd'
 import { UserOutlined } from '@ant-design/icons/lib'
 
 const ZeroHeader: React.FC = () => {
   const { Header } = Layout
   const { Option } = Select
+  const userMenu = (
+    <Menu>
+      <Menu.Item>ryo yoneyama</Menu.Item>
+      <Menu.Divider />
+      <Menu.Item>サインアウト</Menu.Item>
+    </Menu>
+  )
+
   return (
     <Header>
-      <div className="sidebar-user">
-        <h1>ZeroNote</h1>
-        <Select placeholder="ノートを選択">
-          <Option value="note1">ノート１</Option>
-          <Option value="note2">ノート２</Option>
-          <Option value="note3">ノート３</Option>
-        </Select>
+      <h1>ZeroNote</h1>
+      <Select className="select-note" placeholder="ノートを選択" size="large">
+        <Option value="note1">ノート１</Option>
+        <Option value="note2">ノート２</Option>
+        <Option value="note3">ノート３</Option>
+      </Select>
+      <Dropdown className="right-menu" overlay={userMenu}>
         <Avatar size="large" icon={<UserOutlined />} />
-        <span className="user-name">ryo yoneyama</span>
-      </div>
+      </Dropdown>
     </Header>
   )
 }
